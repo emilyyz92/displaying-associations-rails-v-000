26b742005ceea867dc92e5ba2e1bcc744b680a4c
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-02 14:25:20 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,4 @@ clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
 clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
 
 movies = Category.create!(name: "Movies")
+movies.posts.create!(title: "Top 20 Summer blockbusters featuring a cute dog")
